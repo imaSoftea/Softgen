6bb1dae6f5a54a9020fed8633b8a2d5acbd5945e
Carter Thompson <94035527+imaSoftea@users.noreply.github.com>
2023-04-01 01:33:31 -0400

    Setup

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,29 +1,89 @@
-
-/*  Softgen
+/*
+*	Softgen
 *	Carter Thompson
 *
 *	Date Started 3/31/2022
 */
 
+#include <Windows.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
 
+#ifdef __DEBUG
 int main()
+#else
+#pragma warning(disable: 4099)
+int WinMain (_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+#endif
 {
-	GLFWwindow* window;
-
 	#ifdef __DEBUG
-	std::cout << "DEBUG IS ENABLED" << std::endl;
+		std::cout << "~ ~ ~ ~ DEBUG IS ENABLED ~ ~ ~ ~" << std::endl;
 	#endif
 
-	//Initialize the library
+	//Initialize GLFW
 	if (!glfwInit())
 	{	
-		
-
+		#ifdef __DEBUG
+				std::cout << "CANNOT INIT GLFW" << std::endl;
+		#endif
 		return -1;
 	}
+
+	//Create Window
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "CANNOT CREATE WINDOW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	//Make Context
+	glfwMakeContextCurrent(window);
+
+	//Initialize GLEW
+	if (glewInit())
+	{
+		#ifdef __DEBUG
+				std::cout << "CANNOT INIT GLEW" << std::endl;
+		#endif
+		return -1;
+	}
+	#ifdef __DEBUG
+	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+	#endif
+
+	float pos[6] =
+	{
+		-0.5, -0.5,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), pos, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	//Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		//Render
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//Swap Buffers
+		glfwSwapBuffers(window);
+
+		//Poll
+		glfwPollEvents();
+	}
+	glfwTerminate();
 
 	return 0;
 }
